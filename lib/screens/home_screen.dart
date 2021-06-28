90c1c4adf74f6c19ebb5473e38f3c689c4f7f064
Kevin <kevin12kim27@gmail.com>
2021-06-27 22:44:20 -0700

    it works but its ugly

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -76,7 +76,8 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Card(
               child: ListTile(
                 onTap: () {
-                  //Navigator.push(context, '\course', )
+                  Navigator.pushNamed(context, '/course',
+                      arguments: {'course': mFilteredCourses[index]});
                 },
                 title: Text(mFilteredCourses[index].courseName),
               ),
